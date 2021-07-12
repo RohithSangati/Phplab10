1fd3278bf277fc4cf116791a5a0d676efbb3ce8f
RohithSangati <73009657+RohithSangati@users.noreply.github.com>
2021-07-12 11:34:56 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registration Form</title>
+</head>
+<body>
+    <form action="config.php", method="POST">
+        <label>Name:</label><input type="text" name="name"><br>
+        <label>Password:</label><input type="text" name="password"><br>
+        <label>Email:</label><input type="email" name="email"><br>
+        <label>Phone:</label><input type="number" name="phone"><br>
+
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
